f5e327f38bda7f478d7c7f67e0904318f26f1a5b
Jesper <jespernysteen@gmail.com>
2012-11-22 14:35:33 +0100

    hej

---
+++ FlightBooker/src/Customer.java
@@ -0,0 +1,27 @@
+
+public class Customer {
+
+	private String firstName;
+	private String lastName;
+	private String sex;
+	private String CPRnumber;
+	private String passportNumber;
+	private String birthDate;
+	private int customerID;
+	private boolean isGay;	
+	
+	public Customer(String firstName, String lastName, boolean male, String CPRnumber, String passportNumber, String birthDate, int customerID, boolean isGay)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		if(male)
+			sex = "Male";
+			else 
+				sex = "Female";
+		this.CPRnumber = CPRnumber;
+		this.passportNumber = passportNumber;
+		this.birthDate = birthDate;
+		this.customerID = customerID;			
+	}
+	
+}
