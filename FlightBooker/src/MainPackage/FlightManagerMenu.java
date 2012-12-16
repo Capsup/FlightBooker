2ebaa7af9aa16a5cd1f13d0802e38ba90c5d694f
Jonas Kastberg <jihgfee@gmail.com>
2012-12-16 23:20:45 +0100

    SO MUCH COMMENTS

--- FlightBooker/src/MainPackage/FlightManagerMenu.java
+++ FlightBooker/src/MainPackage/FlightManagerMenu.java
@@ -47,6 +47,13 @@ import com.toedter.calendar.JDateChooser;
 import MainPackage.Airport.AirportType;
 import MainPackage.Plane.PlaneType;
 
+/**
+ * A menu that the user can use in order to edit the passenger amount, the 
+ * selected flight aswell as the passengers seats of a selected flight.
+ * 
+ * @author Martin Juul Petersen (mjup@itu.dk), Jesper Nysteen (jnys@itu.dk) and Jonas Kastberg (jkas@itu.dk)
+ *
+ */
 public class FlightManagerMenu
 {
 	private JFrame frame;
@@ -55,7 +62,7 @@ public class FlightManagerMenu
 	private Flight[] displayedFlights;
 	private JPanel[] flightPanels;
 	
-	private Dimension frameSize = new Dimension(1000, 600);
+	private Dimension frameSize;
 	
 	private Reservation currentReservation;
 	private Flight currentFlight;
@@ -63,12 +70,12 @@ public class FlightManagerMenu
 	
 	private JFormattedTextField seatAmountLabel;
 	
-	private FlightPanel planePanel;
+	private FlightPanel flightPanel;
 	
 	private ArrayList<Flight> flights;
 	
 	//Start parameters
-	private int startSeatAmount = 0; 
+	private int startSeatAmount; 
 	
 	private JDateChooser startDateLabel;
 	private JDateChooser endDateLabel;
@@ -84,45 +91,75 @@ public class FlightManagerMenu
 	
 	private int firstPlaneIndex;
 	
+	/**
+	 * This class is initialized by passing it a frame to use, since it is not always
+	 * created with a new JFrame. Since this menu is used in order to create new 
+	 * reserations aswell as editing exsiting ones, we also need to pass it
+	 * the reservation to use. The boolean passed defines whether the
+	 * reservation should be uploaded to the database as a new
+	 * or update an exsisting one.
+	 * @param the frame that everything will be using
+	 * @param the reservation to create or edit
+	 * @param whether or not the reservation is new or not
+	 */
 	public FlightManagerMenu(JFrame frame, Reservation reservation, boolean isNew)
 	{
+		//We save the passed parameters in our fields.
 		this.frame = frame;
 		this.currentReservation = reservation;
 		this.isNew = isNew;
 		
+		//We get every single instance of flights contained in our database, and save them.
 		flights = Database.getInstance().Get( Flight.class );
 		
+		//We initialize an array of displayedFlights, with the same size as the flights gotten.
 		displayedFlights = new Flight[flights.size()];
 		
+		//We initialize the panels for which we wish to display the displayedFlights.
 		flightPanels = new JPanel[displayedFlights.length];
 		
 		if(currentReservation == null)
 		{
+			//If the current reservation is a new one, we set the start seat amount
+			//to 0, and initialize the reservation.
+			
 			startSeatAmount = 0;
 			
 			initializeReservation();
 		}
 		else 
 		{
-			startSeatAmount = currentReservation.getPassengers().length;
+			//We set the starting seat amount to the number of passangers in the current reservation.
 			
+			startSeatAmount = currentReservation.getPassengers().length;
 		}
 		
+		//We setup the frame
 		setupFrame();
 		
+		//We initialize the user interface of the menu
 		makeContent();
 		
+		//We calculate the results of the search inputs. 
 		calculateResults();
-		//updateReservation();
-		
 	}
-	
+
+	/**
+	 * 
+	 * ButtonListener class that allows the listener to respond to events such as clicking on a button or other actions.
+	 * 
+	 * @author Martin Juul Petersen (mjup@itu.dk), Jesper Nysteen (jnys@itu.dk) and Jonas Kastberg (jkas@itu.dk)
+	 * @version 1.0
+	 * 
+	 */
 	class ButtonListener implements ActionListener
 	{
 		int index;
 		
 		public ButtonListener(int i)
 		{
+			//If we press a flight panel we pass the index of the flight to the class.
+			
 			index = i;
 		}
 		
@@ -131,32 +168,41 @@ public class FlightManagerMenu
 			switch(event.getActionCommand())
 			{
 			case "Make Reservation": 	
+				
+				//We check whether we can commit the changes, or there are still parameters left untouched.
 				if(canCommit())
 				{
+					//If this is not a new reservation we make sure to replace the previous reservation in saved in the flight
+					//with the updated one
 					if(!isNew)
 						currentReservation.getFlight().replaceReservation(currentReservation.getCurrentFlightReservationIndex(), currentReservation);
 					
+					//We remove the main panel from the frame, in order to have a clean frame for the new menu to use.
 					frame.remove(mainPanel);
-					new PassengerManagerMenu(frame, currentReservation, isNew);
 					
-					System.out.println(currentReservation.getFlight().getPlane().getPlaneTypeString());
-					//new ReservationInfoMenu(frame, currentReservation);
+					//We initialize a new passenger manager menu, passing the frame, the 
+					//current reservation aswell as if this is a new reservation or not
+					new PassengerManagerMenu(frame, currentReservation, isNew);
 				}
-				break;
-			case "Inspect Reservation": 	
-				System.out.println("MEH!");
-				break;		
+				break;	
 			case "Flight Panel":
-				bottomPanel.remove(planePanel);
+				
+				//We start out by removing the plane panel from the bottom panel, in order to make space for the updated panel
+				bottomPanel.remove(flightPanel);
+				
+				//We wipe the passengers of the current reservation, since they might not fit in the new flight.
 				wipePassengers();
 				
+				//We set the flight assigned to our current reservation to be the flight we pressed.
 				currentReservation.setFlight(flights.get(index));
-				planePanel = new FlightPanel(currentReservation.getFlight(), currentReservation, new Dimension(frameSize.width,(((frameSize.height/3*2)/4)*3)), true);
 				
-				bottomPanel.add(planePanel); 
+				//We initialize a new FlightPanel with the new flight.
+				flightPanel = new FlightPanel(currentReservation.getFlight(), currentReservation, new Dimension(frameSize.width,(((frameSize.height/3*2)/4)*3)), true);
 				
-				updateFlightPanel();
+				//We add the new flightPanel to the bottom panel
+				bottomPanel.add(flightPanel); 
 				
+				//We make sure that the main panel is up to date.
 				mainPanel.invalidate();
 				mainPanel.validate();
 				mainPanel.repaint();
@@ -165,49 +211,64 @@ public class FlightManagerMenu
 		}
 	}
 	
+	/**
+	 * 
+	 * TextListener class that allows the listener to respond to events in which a property has changed.
+	 * 
+	 * @author Martin Juul Petersen (mjup@itu.dk), Jesper Nysteen (jnys@itu.dk) and Jonas Kastberg (jkas@itu.dk)
+	 * @version 1.0
+	 * 
+	 */
 	class TextListener implements PropertyChangeListener
 	{
 		@Override
 		public void propertyChange(PropertyChangeEvent evt) 
 		{
-			//System.out.println(evt.getPropertyName());
-			//if( evt.getPropertyName().equals( "value" ) )
+			//Whenever a property is changed, we wish to calculate the new search results, as well us update our reservation.
 			calculateResults();
-			updateReservation();
+			updatePassengers();
 		}
 	}
 	
+	/**
+	 * 
+	 * TextActionListener class that allows the listener to respond to events in which a text has been clicked.
+	 * 
+	 * @author Martin Juul Petersen (mjup@itu.dk), Jesper Nysteen (jnys@itu.dk) and Jonas Kastberg (jkas@itu.dk)
+	 * @version 1.0
+	 * 
+	 */
 	class TextActionListener implements ActionListener
 	{
 		@Override
         public void actionPerformed( ActionEvent e )
         {
+			//Whenever a text concerning the calculated results is changed, we 
+			//wish to calculate the new search results, as well us update our reservation.
 			calculateResults();
-			updateReservation();
+			updatePassengers();
         }
 	}
 	
+	/**
+	 * calculateResults method that allows the user to calculate the flights shown compared to the search criteria
+	 */
 	private void calculateResults()
 	{
-		//flightPanels = new JPanel[flights.size()];
+		//Since the listModel of the scrollview in which the flights are shown is not initialized
+		//everytime this method is called we need stop the code here.
 		if( listModel == null )
 			return;
 		
 		int height = 0;
 		
-		boolean firstTaken = false;
-		
-		//listModel.clear();
+		//We remove everything already stored in our viewed list in order to make space for the new search results
 		viewedList.removeAll();
+		
+		//We iterate through our flight array in order to check if the flight matches with the search criteria
 		for( int i = 0; i < flights.size(); i++ )
 		{
 			Flight currentFlight = flights.get( i );
-			boolean bSuccess = true;
-			/*System.out.println( "Selected: " + departureList.getSelectedItem().toString());
-			System.out.println( "Currentflight: " + currentFlight.getOrigin().getName());
-			
-			System.out.println( "Selected2: " + destinationList.getSelectedItem().toString());
-			System.out.println( "Currentflight2: " + currentFlight.getDestination().getName());*/
 			
 			boolean bounce = true;
 			
@@ -237,20 +298,20 @@ public class FlightManagerMenu
 			
 			int amount = 0;
 			
+			//if the value of the seatAmount label is not null we retrieve the value
 			if(seatAmountLabel.getValue() != null)
 				amount = Integer.parseInt((String)seatAmountLabel.getValue());
 			
-			
+			//Check if the amount of seats left on the flight is greater than the amount of seats you wish to reserve
 			if(currentFlight.getSeatsLeft() < amount)
 			{
 				bounce = false;
 			}
 			
-			//Martins lange satan
-			//currentFlight.getDate().getTimeInMillis() >= startDateLabel.getCalendar().getTimeInMillis() && currentFlight.getDate().getTimeInMillis() <= endDateLabel.getCalendar().getTimeInMillis() && departureList.getSelectedItem().toString().equals( currentFlight.getOrigin().getName() ) && destinationList.getSelectedItem().toString().equals( currentFlight.getDestination().getName() ) 
-			
+			//If any of the search criteria didnt return false we let the flight be displayed.
 			if(bounce)
 			{
+				//we initialize a new JPanel with the informations of the current flight
 				JPanel flightPanel = setupFlightPanel( currentFlight );
 				
 				JButton flightPanelButton = new JButton();
@@ -260,44 +321,21 @@ public class FlightManagerMenu
 				
 				flightPanelButton.addActionListener(new ButtonListener(i));
 				
-				if(!firstTaken)
-				{
-					firstTaken = true;
-					firstPlaneIndex = i;
-				}
-				
+				//We add the height of the new button to the collected height of the new list.
 				height += flightPanelButton.getPreferredSize().height;
 				
+				//We add the flight button to our list.
 				viewedList.add(flightPanelButton);
-				
-//				if( flightPanel != null )
-//				{
-//					JButton test = new JButton();
-//					test.add(flightPanel);
-//					
-//					viewedList.add(test);
-//					//listModel.addElement( test );
-//				}
-				
-				//viewedList.repaint();
-				//scrollPane.repaint();
 			}
 		}
 		
+		//We add a last margin for the list to look nice
 		height += 5;
 		
+		//We set the new height of the viewed list to the calculated size.
 		viewedList.setPreferredSize(new Dimension(200,height));
-		/*
-		bottomPanel.remove(planePanel);
-		wipePassengers();
 		
-		currentReservation.setFlight(flights.get(firstPlaneIndex));
-		planePanel = new PlanePanel(currentReservation.getFlight(), currentReservation, new Dimension(frameSize.width,(((frameSize.height/3*2)/4)*3)), true);
-		
-		bottomPanel.add(planePanel); 
-		
-		updateFlightPanel();
-		*/
+		//We make sure that the main panel is up to date.
 		mainPanel.invalidate();
 		mainPanel.validate();
 		mainPanel.repaint();
@@ -306,6 +344,8 @@ public class FlightManagerMenu
 	
 	private void setupFrame()
 	{
+		frameSize = new Dimension(1000, 600);
+		
 		frame.setSize(frameSize.width, frameSize.height);
 		frame.setResizable(false);
 		frame.setTitle("New Reservation Menu");
@@ -468,22 +508,20 @@ public class FlightManagerMenu
 		bottomPanel.setPreferredSize(new Dimension(frameSize.width, frameSize.height/3*2));
 		
 			//Plane Panel
-		planePanel = new FlightPanel(currentReservation.getFlight(), currentReservation, new Dimension(frameSize.width,(((frameSize.height/3*2)/4)*3)), true);
+		flightPanel = new FlightPanel(currentReservation.getFlight(), currentReservation, new Dimension(frameSize.width,(((frameSize.height/3*2)/4)*3)), true);
 		
 			//Button Panel
 		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		buttonPanel.setPreferredSize(new Dimension(frameSize.width, (frameSize.height/3*2/4)));
 		
 		JButton makeReservationButton = new JButton("Make Reservation");
-		JButton inspectReservationButton = new JButton("Inspect Reservation");
 		JButton closeButton = new JButton("Close");		
 		
 		buttonPanel.add(makeReservationButton);
-		buttonPanel.add(inspectReservationButton);
 		
 		//bottomPanel.add(planePanel, BorderLayout.CENTER);
 		bottomPanel.add(buttonPanel, BorderLayout.SOUTH);
-		bottomPanel.add(planePanel);
+		bottomPanel.add(flightPanel);
 		
 		
 		//Finish up
@@ -497,12 +535,15 @@ public class FlightManagerMenu
 		ButtonListener listener = new ButtonListener(0);
 		
 		makeReservationButton.addActionListener(listener);
-		inspectReservationButton.addActionListener(listener);
 		
 		update();
-		//updateReservation();
 	}
 	
+	/**
+	 *  The setupFlightPanel class setups a flight panel with any valid data to display taken from the passed flight
+	 * @param flight to make the panel from
+	 * @return A JPanel that has been setup according to the passed flight.
+	 */
 	JPanel setupFlightPanel(Flight currentFlight)
 	{
 		//Flight Panel
@@ -602,60 +643,51 @@ public class FlightManagerMenu
 		frame.setVisible(true);
 	}
 	
+	/**
+	 * initializeReservation method initializes the currentReservation. This is done since many of the methods used
+	 * to change the reservation requires non-null instances of the different fields.
+	 */
 	private void initializeReservation()
 	{
-		/*
-		currentReservation = new Reservation(null,  
-						null, 
-						null, 
-						new Passenger[startSeatAmount],
-						0);*/
-		
 		currentReservation = new Reservation();
 		
 		currentReservation.setPassengers(new Passenger[startSeatAmount]);
 		
 		currentFlight = flights.get(0);
 		
-		Passenger[] passengerArray = currentReservation.getPassengers();
-		
-		for(int i=0; i<passengerArray.length; i++)
-		{
-			passengerArray[i] = new Passenger(null, null);
-		}
-		
-		currentReservation.setPassengers(passengerArray);
 		currentReservation.setFlight(currentFlight);
-		
 	}
 	
-	private void updateReservation()
-	{
-		//currentReservation.setFlight(currentFlight);
-		
-		updatePassengers();
-		
-	}
-	
+	/**
+	 * the updatePassenger method updates the passenger array accordingly to the amount of seats chosen.
+	 */
 	private void updatePassengers()
 	{
 		int amount = 0;
 		
+		//We will only update our passengers if the value of the seat amount label is not null
 		if(seatAmountLabel.getValue() != null)
 		{
-			
+			//The length of the new passenger array is defined by the seat amount label.
 			amount = Integer.parseInt((String)seatAmountLabel.getValue());
 		
-			Passenger[] passengerArray = new Passenger[amount];
+			//We initialize the new array with the correct amount.
+			Passenger[] newPassengerArray = new Passenger[amount];
 			
+			//We initialize an array that holds the current reservations passengers.
 			Passenger[] currentPassengerArray = currentReservation.getPassengers();
 			
-			for(int i=0; i<passengerArray.length; i++)
+			//We iterate through the new passenger array to make sure the new array contains the previously
+			//selected seats and passengers.
+			for(int i=0; i<newPassengerArray.length; i++)
 			{
+				//As long as the index is less that the current passenger array we
+				//set the passenger of the new passenger array to the one of the current passenger array.
 				if(i<currentPassengerArray.length)
 				{
 					int index = i;
 					
+					//We go through the current passenger array until we find a passenger that is not null
 					while(currentPassengerArray[index] == null && index < currentPassengerArray.length)
 					{
 						index += 1;
@@ -663,37 +695,49 @@ public class FlightManagerMenu
 					
 					if(currentPassengerArray[index] != null)
 					{
-						passengerArray[i] = currentPassengerArray[index];
+						//We assign the passenger from the current passenger array to the one of the new passenger array.
+						newPassengerArray[i] = currentPassengerArray[index];
 					}
 					else 
 					{
-						passengerArray[i] = new Passenger(null, null);
+						//If we did not find any passenger in the current passenger array we initialize a new one for the new passenger array.
+						newPassengerArray[i] = new Passenger(null, null);
 					}
 				}
 				else 
 				{
-					passengerArray[i] = new Passenger(null, null);
+					//If we exceed the length that the current passenger array had, we initialize null passengers for the remained indexes.
+					newPassengerArray[i] = new Passenger(null, null);
 				}
 			}
 			
-			//Skal opdateres og fixes
-			if(currentReservation.getPassengers().length > passengerArray.length)
+			//If we reduce the length of the passenger array we make sure that none of the
+			//exceeding indexes seats are still left as occupied.
+			if(currentReservation.getPassengers().length > newPassengerArray.length)
 			{
-				int start = passengerArray.length;
+				int start = newPassengerArray.length;
 				
+				//We iterate through any seat that is not a part of the new passenger array.
 				for(int i=start; i < currentReservation.getPassengers().length; i++)
 				{	
+					//If the seat was occupied we make sure that it changes its booking status to false.
 					if(currentReservation.getPassengers()[i].getSeat() != null)
 						currentReservation.getPassengers()[i].getSeat().changeBookingStatus(false);
 				}
 			}
 			
-			currentReservation.setPassengers(passengerArray);
+			//We assign the new passenger array to the current reservation.
+			currentReservation.setPassengers(newPassengerArray);
 			
+			//We update the flight panel, to make sure that the data shown corresponds to that which is saved
+			//in the current reservation.
 			updateFlightPanel();
 		}
 	}
 	
+	/**
+	 * The wipePassengers method is used to remove any seat from the current reservation.
+	 */
 	void wipePassengers()
 	{
 		Passenger[] passengers = currentReservation.getPassengers();
@@ -704,12 +748,19 @@ public class FlightManagerMenu
 		}
 	}
 	
+	/**
+	 * The updateFlightPanel method is used to update the flight panel seats.
+	 */
 	void updateFlightPanel()
 	{
-		if(planePanel != null)
-			planePanel.updateSeats();
+		if(flightPanel != null)
+			flightPanel.updateSeats();
 	}
 	
+	/**
+	 * The canCommit method returns whether or not you have filled out all viable data of the menu.
+	 * @return
+	 */
 	boolean canCommit()
 	{
 		boolean returnBool = true;
@@ -718,11 +769,14 @@ public class FlightManagerMenu
 		
 		if(passengers.length > 0)
 		{
+			//We iterate through every passenger of the reservtion and check that they have a specified seat
 			for (Passenger passenger : passengers) 
 			{
 				if(passenger.getSeat() == null)
 				{
 					returnBool = false;
+					
+					//if one of the passenger has not selected a seat we show an error message, telling the user to select one.
 					showErrorDialog();
 				}
 			}
@@ -735,6 +789,9 @@ public class FlightManagerMenu
 		return returnBool;
 	}
 	
+	/**
+	 * The showErrorDialog displays a dialog telling the user to fill out any given information, before continuing.
+	 */
 	private void showErrorDialog()
 	{
 		JOptionPane.showMessageDialog(frame,
--- FlightBooker/src/MainPackage/SeatButton.java
+++ FlightBooker/src/MainPackage/SeatButton.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+
 public class SeatButton extends JButton
 {
 	private Seat seat;
